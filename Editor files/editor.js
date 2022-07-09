0b935dcc4f7a9742292a26a46fa968f63a566b27
Enakkhi Saha <sahaenakkhi@gmail.com>
2022-07-09 20:25:08 +0530

    fixed download feature

--- Editor files/editor.js
+++ Editor files/editor.js
@@ -4,6 +4,7 @@ let codeEditor2 = ace.edit("cssCode");
 let defaultCode2 = '/*enter your css*/';
 let codeEditor3 = ace.edit("jsCode");
 let defaultCode3 = '//enter your js';
+var content="";
 
 const executeCodeBtn = document.querySelector('.editor_btn');
 
@@ -13,6 +14,27 @@ const executeCodeBtn = document.querySelector('.editor_btn');
 //     res.write(codeEditor.getValue());
 //     res.close;
 // }
+
+function saveProject(){
+    var projectName = document.getElementById("projectname").value;
+    if(projectName.trim() == "" || projectName == null){
+        alert("Project name cannot be blank.");
+        return;}
+    else
+
+ var cssContent = codeEditor2.getValue();
+ var jsContent = codeEditor3.getValue();
+ var htmlContent =  codeEditor.getValue();
+
+ var obj={};
+ obj.css=cssContent;
+ obj.js=jsContent;
+ obj.html=htmlContent;
+
+ localStorage.setItem(projectName,JSON.stringify(obj));
+alert(":-) Project is saved successfully");
+
+}
 
 let editorLib ={
     init(){
@@ -60,12 +82,17 @@ let editorLib ={
     
 }
 
+
+
+
+
 //events
 executeCodeBtn.addEventListener('click', () =>{
     //get input from the code editor
     const userCode = codeEditor.getValue();
     const userCode2 = codeEditor2.getValue();
     const userCode3 = codeEditor3.getValue();
+    
 
     var res=document.getElementById('preview-window').contentWindow.document;
     res.open();
@@ -78,6 +105,7 @@ executeCodeBtn.addEventListener('click', () =>{
 
     // res.open();
     res.write('<script>'+userCode3+'</script>');
+    content="\n"+userCode+'\n\n<style>\n'+userCode2+'\n</style>\n'+'\n<script>\n'+userCode3+'\n</script>\n';
     res.close;
 
     //run the user code
@@ -89,6 +117,81 @@ executeCodeBtn.addEventListener('click', () =>{
         console.error(err);
     }
 });
+
+
+//downloading and saving
+window.onload=function(){
+    var html="<option></option>";
+    for(var i=0;i<localStorage.length;i++){
+html+="<option>" +localStorage.key(i)+"</option>";
+    }
+
+    document.getElementById("projects").innerHTML=html;
+}
+
+
+
+
+        window.addEventListener("keydown",function(e){
+
+           if((event.ctrlKey || event.metaKey) && (e.key == 's' || e.key == 'S')){
+            e.preventDefault();
+            
+            var filename = prompt("Enter file name(with extension like .html or .htm or .txt): ");
+            if(filename.trim() == "" || filename == null){
+               alert("Project name cannot be blank.");
+               return;}
+           else
+            var a =document.createElement("a");
+            a.href = "data:application/octet-stream, " + encodeURIComponent(
+                "<html>\n" + content+ "\n</html>" );
+                a.download=filename;
+               a.click();
+           }
+        })
+
+        function download(){
+           var filename = prompt("Enter file name(with extension like .html or .htm or .txt) : ");
+           if(filename.trim() == "" || filename == null){
+               alert("Project name cannot be blank.");
+               return;}
+           else
+           var a =document.createElement("a");
+           a.href = "data:application/octet-stream, " + encodeURIComponent(
+               "<html>" + content+ "<\/html>" );
+               a.download=filename;
+              a.click();
+        }
+
+
+
+// document.querySelector("#run-btn").addEventListener("click",function(){
+//     content="";
+//     let htmlCode = htmlEditor.getValue();
+//     let cssCode = "<style>\n"+ cssEditor.getValue() + "\n</style>";
+//     let jsCode = "<scri"+"pt>\n"+ jsEditor.getValue() + "\n</scri"+"pt>";
+//     let previewWindow = document.querySelector("#preview-window").contentWindow.document;
+//     previewWindow.open();
+//     previewWindow.write(htmlCode+cssCode+jsCode);
+//     content =htmlCode+cssCode+jsCode;
+//     previewWindow.close()
+// })
+
+
+
+
+
+// document.querySelector("#run-btn").addEventListener("click",function(){
+//     content="";
+//     let htmlCode = htmlEditor.getValue();
+//     let cssCode = "<style>\n"+ cssEditor.getValue() + "\n</style>";
+//     let jsCode = "<scri"+"pt>\n"+ jsEditor.getValue() + "\n</scri"+"pt>";
+//     let previewWindow = document.querySelector("#preview-window").contentWindow.document;
+//     previewWindow.open();
+//     previewWindow.write(htmlCode+cssCode+jsCode);
+//     content =htmlCode+cssCode+jsCode;
+//     previewWindow.close()
+// })
 
 
 editorLib.init();
